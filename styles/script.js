67c8a4d5fd6d1985ebb8682a0ad0d8c6637d5525
DustinHK <dustinkopalit9@gmail.com>
2023-10-16 18:47:17 +0800

    UPDET LOGIN REGIS DASHBOARD DKK

---
+++ styles/script.js
@@ -0,0 +1,4 @@
+function toggleSidebar() {
+    const sidebarContent = document.getElementById('sidebarContent');
+    sidebarContent.style.display = (sidebarContent.style.display === 'none' || sidebarContent.style.display === '') ? 'block' : 'none';
+}
