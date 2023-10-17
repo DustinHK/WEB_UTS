12c3f289299e1f4fd3e6940d001e0dde0d065aa8
nabilsaragih <nabilsaragih33@gmail.com>
2023-10-17 21:11:36 +0800

    feat: premature form

--- scripts/php/req_form.php
+++ scripts/php/req_form.php
@@ -4,27 +4,28 @@ require 'req_login.php';
 
 session_start();
 
+$userData = $_SESSION["userData"];
+$username = $userData["username"];
+
 if (isset($_POST['submit'])) {
     $nama = $_POST['nama'];
-    $alamat = $_POST['alamat'];
-    $email = $_POST['email'];
-    $jenjang = $_POST['jenjang'];
     $nim = $_POST['nim'];
     $prodi = $_POST['prodi'];
-    $ipk = $_POST['ipk'];
-    $angkatan = $_POST['angkatan'];
-    $kontak = $_POST['kontak'];
+    $jenjang = $_POST['jenjang'];
     $tempat_lahir = $_POST['tempat_lahir'];
+    $alamat = $_POST['alamat'];
+    $no_telepon = $_POST['no_telepon'];
     $tanggal_lahir = $_POST['tanggal_lahir'];
-    $jk = $_POST['jk'];
+    $ipk = $_POST['ipk'];
+    $email = $_POST['email'];
+    $angkatan = $_POST['angkatan'];
+    $semester = $_POST['semester'];
 
-    $query = 
-            "INSERT INTO user (id_user, nim, username, password, nama_lengkap) 
-            VALUES ('', '$nim', '$username', '$password', '$nama')";
+    $query = "UPDATE user SET nama=$nama, nim=$nim, prodi=$prodi, jenjang=$jenjang, tempat_lahir=$tempat_lahir, alamat=$alamat, no_telepon=$no_telepon, tanggal_lahir=$tanggal_lahir, ipk=$ipk, email=$email, angkatan=$angkatan, semester=$semester WHERE username=$username";
     $result = mysqli_query($conn, $query);
 
     if ($result) {
-        header('Location: ../../index.php');
+        header('Location: ../../pages/main.php');
         exit;
     } else {
         $error = true;
